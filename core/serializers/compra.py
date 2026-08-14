6397aae11a096670ead1d63440ee40c02bb59226
GabrielaSenderski <gabrielasenderski.ifc@gmail.com>
2026-08-14 09:25:06 -0300

    feat: Visualização dos itens da compra no endpoint da listagem de compras

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -1,13 +1,25 @@
+from decimal import Decimal
+
+from dill import source
+from rest_framework.fields import DecimalField
 from rest_framework.serializers import CharField, ModelSerializer
 
 from core.models import Compra, ItensCompra
 
 
 class ItensCompraSerializer(ModelSerializer):
+    titulo = CharField(source='livro.titulo', read_only=True)
+    editora = CharField(source='livro.editora.nome', read_only=True)
+    preco = DecimalField(
+        source='livro.preco',
+        max_digits=7,
+        decimal_places=2,
+        read_only=True,
+    )
+    capa = CharField(source='livro.capa.url', read_only=True)
     class Meta:
         model = ItensCompra
-        fields = ('livro', 'quantidade')
-        depth = 1
+        fields = ('id', 'titulo', 'editora', 'quantidade', 'preco', 'capa')
 
 
 class CompraSerializer(ModelSerializer):
